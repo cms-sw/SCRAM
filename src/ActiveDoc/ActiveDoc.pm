da4ed2a3ecdbb526648b3a39c81f90dc222f19f8
Christopher Williams <sha1-ebcb402bd5472f352dd1d1c7a94fa0cbb49cf4a3\@cern.ch>
1999-09-20 16:27:57 +0000

    Basic Working Level

--- src/ActiveDoc/ActiveDoc.pm
+++ src/ActiveDoc/ActiveDoc.pm
@@ -5,8 +5,8 @@
 # --------
 # Interface
 # ---------
-# new(filename, ObjectStoreCont): create a new object based on a file and
-#                                 associate with the given ObjectStoreCont
+# new(filename, DOChandler): create a new object based on a file and
+#                                 associate with a base DOChandler
 # parse()			: parse the input file
 # include(url) : Activate include file mechanism, returns the object ref if OK
 # treenode()   : return the associated TreeNode object reference
@@ -14,46 +14,70 @@
 #                         included objects
 # find(string)	: find the object reference related to string in the associated
 #		  tree. Mechanism for getting object references
+# _addgroup()	: Add group functionality to document
+# parseerror(String) : Report an error to the user
+# userinterface()	: return the default User Interface object
+# checktag($hashref, param , tagname) : Check a hash returned from switcher
+#					for a given parameter
 
-
-package ActiveDoc;
-use BaseTags;
-use DOChandler;
+package ActiveDoc::ActiveDoc;
+require 5.001;
+use ActiveDoc::DOChandler;
+use ActiveDoc::TreeNode;
+use ActiveDoc::UserQuery;
 use ObjectStoreCont;
 
-@ISA = qw (BaseTags);
+@ISA = qw(BaseTags);
 
 # Initialise
 sub _init {
 	my $self=shift;
+	my $DOChandler=shift;
 	my $OC=shift;
 
 	$self->_addurl();
-	$self->{OC}=$OC;
-	$self->{treenode)=TreeNode->new();
-	$self->{includeOS}=$self->{OC}->newStore();
-        $self->{dochandler}=DOChandler->new($self->{includeOS});
+	$self->{urlhandler}->setcache($DOChandler->defaultcache());
+	$self->{treenode}=ActiveDoc::TreeNode->new();
+        $self->{dochandler}=$DOChandler;
+	$self->{UserQuery}=$DOChandler->{UserQuery};
+        $self->{tags}->addtag("Use", \&Use_Start, "", "");
 	# Add the minimal functionality tag - feel free to override
         $self->{tags}->addtag("Include", \&Include_Start, "", "");
+	$self->init();
 }
 
+sub init {
+	# Dummy Routine - override for derrived classes
+}
 #
-# Include mechanism
+# use mechanism
 #
 sub include {
         my $self=shift;
         my $url=shift;
+	my $linkfile=shift;
+	my $filename;
         my $obj;
 
-        $obj=$self->{dochandler}->newdoc($url);
+	$file=$self->{urlhandler}->get($url);
+	if ( $linkfile ne "" ) {
+	  $filename=$file."/".$linkfile;
+	}
+        $obj=$self->{dochandler}->newdoc($filename);
+
 	# Now Extend our tree
 	$self->{treenode}->grow($obj->treenode());
         return $obj;
 }
 
+sub userinterface {
+	my $self=shift;
+	return $self->{dochandler}->{UserInterface};
+}
+
 sub treenode {
 	my $self=shift;
-	return $self->treenode;
+	return $self->{treenode};
 }
 
 sub getincludeObjectStore {
@@ -64,33 +88,54 @@ sub getincludeObjectStore {
 sub find($) {
 	my $self=shift;
 	my $string=shift;
+	my $tn;
 
-	$self->{treenode}->find($string);
+	$tn=$self->{treenode}->find($string);
+	if ( $tn eq "" ) {
+	  $self->parseerror("Unable to find $string");
+	}
+	return $tn->associate();
+}
+
+sub parseerror {
+	my $self=shift;
+	my $string=shift;
+
+	print "Parse Error in $self->{url}, line $self-{switch}->line()\n";
+	print $string."\n";
+	die;
+}
+
+sub checktag {
+	my $self=shift;
+	my $hashref=shift;
+	my $param=shift;
+	my $tagname=shift;
+
+	if ( ! exists $$hashref{$param} ) {
+	  $self->parseerror("Incomplete Tag <$tagname> : $param required");  
+	}
 }
 
 # ------------------------ Tag Routines ------------------------------
 #
-# A default Include tag
+# The Include tag
 #
+
 sub Include_Start {
-        my $returnval;
-        # Just call the basic - this is only a default wrapper for the
-        # <INCLUDE> tag assuming with no futher processing of the DOCObjref
-        $returnval=_includetag(@_)
-        # dont return anything if its a basic tag
+	my $self=shift;
+	my $name=shift;
+	my $hashref=shift;
+
+        $self->{switch}->checkparam( $name, "ref");
+	print "<Include> tag not yet implemented\n";
+#        $self->include($$hashref{'ref'},$$hashref{'linkdoc'});
 }
 
-# ----------------------- Support Routines ---------------------------
-#
-# the real workings of the include tag returns the ref
-#
-sub _includetag {
-        my $self=shift;
+sub Use_Start {
+	my $self=shift;
         my $name=shift;
         my $hashref=shift;
 
-        $self->{switch}->checkparam( $name, "ref");
-        $url=$$hashref{'ref'};
-        return $self->include($url);
+	print "<Use> tag not yet implemented\n";
 }
-
