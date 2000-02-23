3a2febde8e9b3caee09999fd427741eb77cd7df3
Christopher Williams <sha1-ebcb402bd5472f352dd1d1c7a94fa0cbb49cf4a3\@cern.ch>
2000-02-23 13:24:13 +0000

    Use ActiveDoc_arch

--- src/Configuration/ConfigArea.pm
+++ src/Configuration/ConfigArea.pm
@@ -4,6 +4,13 @@
 # Originally Written by Christopher Williams
 #
 # Description
+# -----------
+# creates and manages a configuration area
+#
+# Options
+# -------
+# ConfigArea_location
+# ConfigArea_name
 #
 # Interface
 # ---------
@@ -19,14 +26,25 @@
 # configitem(@keys)		: return a list of fig items that match
 #				  the keys - all if left blank
 # parentstore()			: set/return the parent ObjectStore
+# basearea(ConfigArea)		: Set/Get the base area
+# freebase()			: Remove any link to a base area
 # bootstrapfromlocation([location]): bootstrap the object based on location.
 #				  no location specified - cwd used
 # searchlocation([startdir])	: returns the location directory. search starts
 #				  from cwd if not specified
 # defaultdirname()		: return the default directory name string
 # copy(location)		: make a copy of the current area at the 
-#				  specified location - return an object
-#				  representing the area
+#				  specified location - defaults to cwd/default
+#				  if not specified . ConfigArea_name, 
+#				  ConfigArea_location also override .
+#				  Return an object representing the area
+# satellite()			: make a satellite area based on $self
+# arch([archobj])		: Set/get the architecture object
+# structure(name)		: return the object corresponding to the
+#				  structure name
+# structurelist()		: return list of structure objectS
+# downloadtotop(dir,url)	: download the url to a dir in the config area
+#				  
 
 package Configuration::ConfigArea;
 use ActiveDoc::ActiveDoc;
@@ -34,8 +52,9 @@ require 5.004;
 use Utilities::AddDir;
 use ObjectUtilities::ObjectStore;
 use Configuration::ConfigStore;
+use Configuration::ActiveDoc_arch;
 use Cwd;
-@ISA=qw(ActiveDoc::ActiveDoc ObjectUtilities::StorableObject);
+@ISA=qw(Configuration::ActiveDoc_arch ObjectUtilities::StorableObject);
 
 sub init {
 	my $self=shift;
@@ -46,31 +65,53 @@ sub init {
 	$self->addtag("init","project",\&Project_Start,$self,
 	    \&Project_text,$self,"", $self );
 	$self->addurltags("download");
+	$self->addtag("download","download",\&Download_Start,$self, 
+						"", $self, "",$self);
 	$self->addtag("download","use",\&Use_download_Start,$self, 
 						"", $self, "",$self);
 	$self->addurltags("setup");
 	$self->addtag("setup","use",\&Use_Start,$self, "", $self, "",$self);
+	$self->addtag("setup","structure",\&Structure_Start,$self,
+			 "", $self, "",$self);
 
 	# data init
 	$self->{admindir}=".SCRAM";
 }
 
+sub basearea {
+	my $self=shift;
+
+	my $area;
+	if ( @_ ) {
+	  $area=shift;
+	  $self->config()->store($area,"BaseArea");
+	}
+	else {
+	  ($area)=$self->config()->restore("BaseArea");
+	}
+	return $area;
+}
+
+sub freebase {
+	my $self=shift;
+	$self->config()->delete("BaseArea");
+}
 
 sub defaultdirname {
 	my $self=shift;
         my $name=$self->name();
         my $vers=$self->version();
-        $vers=~s/^$name_//;
+        $vers=~s/^$name\_//;
         $name=$name."_".$vers;
         return $name;
-
 }
+
 
 sub setup {
 	my $self=shift;
 
 	# --- find out the location - default is cwd
-	my $location=$self->option("area_location");
+	my $location=$self->option("ConfigArea_location");
 	if ( ! defined $location ) {
 	        $location=cwd();
 	}
@@ -79,7 +120,7 @@ sub setup {
 	}
 
 	# --- find area directory name , default name projectname_version
-	my $name=$self->option("area_name");
+	my $name=$self->option("ConfigArea_name");
 	if ( ! defined $name ) {
 	  $name=$self->defaultdirname();
 	}
@@ -89,7 +130,6 @@ sub setup {
 	$self->_setupstore();
 
 	# --- download everything first
-# FIX-ME --- cacheing is broken
 	$self->parse("download");
 	
 	# --- and parse the setup file
@@ -103,12 +143,23 @@ sub setup {
 							$self->version());
 }
 
+sub structure {
+	my $self=shift;
+	my $vr=shift;
+	return $self->{structures}{$vr};
+}
+
+sub structurelist {
+	my $self=shift;
+	return ( keys %{$self->{structures}} );
+}
+
 sub _setupstore {
 	my $self=shift;
 
 	# --- make a new ConfigStore at the location and add it to the db list
 	my $ad=Configuration::ConfigStore->new($self->location().
-				"/".$self->{admindir});
+				"/".$self->{admindir}, $self->arch());
 
 	$self->parentconfig($self->config());
 #        $self->config(Configuration::ConfigureStore->new());
@@ -147,29 +198,43 @@ sub store {
 	$self->savevar($fh,"name", $self->name());
 	$self->savevar($fh,"version", $self->version());
 	$fh->close();
+
+	$self->_storestructures();
+}
+
+sub satellite {
+	my $self=shift;
+	my $newarea=$self->copy(@_);
+	$newarea->_makesatellites();
+	return $newarea;
 }
 
 sub copy {
 	my $self=shift;
-	my $destination=shift;
 	use File::Basename;
 	# create the area
 
-	AddDir::adddir(dirname($destination));
+	my $destination;
+	if ( @_ ) {
+	 $destination=shift;
+	}
+	else {
+	  my($location,$name)=$self->_defaultoptions();
+	  $destination=$location."/".$name
+	}
+	#AddDir::adddir(dirname($destination)."/".$self->{admindir});
+	#AddDir::adddir($destination."/".$self->{admindir});
 	
-	$temp=$self->location();
-	my @cpcmd=(qw(cp -r), "$temp", "$destination");
-	print "@cpcmd"."\n";
-#	File::Copy::copy("$self->location()", "$destination") or 
-	system(@cpcmd) == 0 or
-			$self->error("Cannot copy ".$self->location().
-			" to $destination ".$!);
-
+	# copy across the admin dir
+	$temp=$self->location()."/".$self->{admindir};
+	AddDir::copydir($temp,"$destination/".$self->{admindir});
 	# create a new object based on the new area
 	my $newarea=ref($self)->new($self->parentconfig());
 	$newarea->bootstrapfromlocation($destination);
 	# save it with the new location info
-	$newarea->store($self->location()."/".$self->{admindir}."/ConfigArea.dat");
+	$newarea->store($self->location()."/".$self->{admindir}.
+							"/ConfigArea.dat");
+	return $newarea;
 }
 
 sub restore {
@@ -187,6 +252,8 @@ sub restore {
         $self->name($$varhash{"name"});
         $self->version($$varhash{"version"});
         $fh->close();
+
+	$self->_restorestructures();
 }
 
 sub name {
@@ -211,7 +278,7 @@ sub location {
 	}
 	elsif ( ! defined $self->{location} ) {
 	  # try and find the release location
-	  #$self->{location}=$self->searchlocation();
+	  $self->{location}=$self->searchlocation();
 	}
 	return  $self->{location};
 }
@@ -263,6 +330,61 @@ sub addconfigitem {
 #	$self->config()->storepolicy("local");
 }
 
+sub downloadtotop {
+	my $self=shift;
+	my $url=shift;
+	my $dir=shift;
+	
+	# only download once
+	if ( ! -e $self->location()."/".$dir ) { 
+	  $self->{urlhandler}->download($url,$self->location()."/".$dir);
+	}
+}
+
+sub _makesatellites {
+	my $self=shift;
+	foreach $st ( values %{$self->{structures}} ) {
+	   $st->setupsatellite()
+	}
+}
+
+sub _storestructures {
+	my $self=shift;
+	foreach $struct ( values %{$self->{structures}} ) {
+	  $self->config()->store($struct, "Structures", $struct->name());
+	}
+}
+
+sub _restorestructures {
+	my $self=shift;
+	my @strs=$self->config()->find("Structures");
+	foreach $struct ( @strs ) {
+	  $struct->parent($self);
+	  $self->{structures}{$struct->name()}=$struct;
+	}
+}
+
+sub _defaultoptions {
+	my $self=shift;
+	my $name;
+	my $location;
+
+	# --- find out the location - default is cwd
+        $location=$self->option("ConfigArea_location");
+        if ( ! defined $location ) {
+                $location=cwd();
+        }
+        elsif ( $location!~/^\// ) {
+                $location=cwd()."/".$location;
+        }
+
+        # --- find area directory name , default name projectname_version
+        $name=$self->option("ConfigArea_name");
+        if ( ! defined $name ) {
+          $name=$self->defaultdirname();
+        }
+	return ($location,$name);
+}
 # -------------- Tags ---------------------------------
 # -- init parse
 sub Project_Start {
@@ -288,6 +410,21 @@ sub Project_text {
 
 # ---- download parse
 
+sub Download_Start {
+	my $self=shift;
+        my $name=shift;
+        my $hashref=shift;
+
+	$self->checktag($name,$hashref,'url');
+	$self->checktag($name,$hashref,'location');
+	if ( $$hashref{'location'}!~/^\w/ ) {
+	  $self->parseerror("location must start with an".
+		" alphanumeric character");
+	}
+	print "Downloading .... ".$$hashref{'url'}."\n";
+	$self->downloadtotop($$hashref{'url'},$$hashref{'location'});
+}
+
 sub Use_download_Start {
 	my $self=shift;
 	my $name=shift;
@@ -299,6 +436,41 @@ sub Use_download_Start {
 }
 
 # --- setup parse
+
+sub Structure_Start {
+	my $self=shift;
+        my $name=shift;
+        my $hashref=shift;
+
+	$self->checktag($name,$hashref,'name');
+	if ( !( exists $$hashref{'type'}) || ( exists $$hashref{'url'}) ) {
+	    $self->parseerror("No url or type given in <$name> tag");
+	}
+	if ( ! exists $self->{structures}{$$hashref{'name'}} ) {
+	  if ( exists $$hashref{'type'}) {
+	    # create a new object of the specified type
+	    eval "require $$hashref{'type'} ";
+	    if  ( $@ ) {
+		$self->parseerror("Unable to instantiate type=".
+			$$hashref{'type'}." in <$name> .".$@);
+	    }
+	    $self->{structures}{$$hashref{'name'}}=
+		$$hashref{'type'}->new($self->config());
+	    $self->{structures}{$$hashref{'name'}}->name($$hashref{'name'});
+	    $self->{structures}{$$hashref{'name'}}->parent($self);
+	    $self->{structures}{$$hashref{'name'}}->vars($hashref);
+	  }
+	  else { # its an activedoc
+		$self->{structures}{$$hashref{'name'}}=
+				$self->activatedoc($$hashref{'url'});
+	  }
+	  $self->{structures}{$$hashref{'name'}}->setupbase();
+	}
+	else {
+	     $self->parseerror("Multiply defined Structure - ".
+							$$hashref{'name'});
+	}
+}
 
 sub Use_Start {
 	my $self=shift;
