41347df6d5827e1eba2196aa34cd47dd3860d399
Shahzad Malik Muzaffar <sha1-dc0e3810c4d4d85d6295265f1fb210880ec3f132@cern.ch>
2012-05-23 10:30:35 +0000

    check for .SCRAM/${arch}/timestamps/self file for a valid release area

--- src/SCRAM/ScramProjectDB.pm
+++ src/SCRAM/ScramProjectDB.pm
@@ -260,7 +260,7 @@ sub _findProjects()
       foreach my $fd (glob($db))
       {
         if (!-d $fd){next;}
-	if (($valid) && (!-d "${fd}/.SCRAM/${arch}/MakeData")){next;}
+	if (($valid) && (!-d "${fd}/.SCRAM/${arch}/timestamps/self")){next;}
 	my $d=basename($fd);
 	if ($d=~/^$ver$/)
 	{
