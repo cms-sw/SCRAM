47a8a6531741099b71a5561f18872218809eaf1c
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-04-12 14:43:02 +0200

    use manxcpu for -j0

--- src/BuildSystem/MakeInterface.pm
+++ src/BuildSystem/MakeInterface.pm
@@ -28,7 +28,35 @@ sub exec()
    my $self=shift;
    my ($makefile)=@_;
    my $arg="";
-   foreach my $a (@ARGV){$arg.=" '$a'";}
+   my $job_args=0;
+   my $job_val="";
+   foreach my $a (@ARGV)
+     {
+     if($a=~/^(-j|--jobs=)([0-9]*)$/o)
+       {
+       $job_args=1;
+       $job_val=$2;
+       next;
+       }
+     if(($job_args) && ($job_val eq ""))
+       {
+       if ($a=~/^[0-9]+$/){$job_val=$a; next;}
+       else{$job_val="0";}
+       }
+     $arg.=" '$a'";
+     }
+   if($job_args)
+     {
+     if($job_val=~/^(0+|)$/o)
+       {
+       my $os="$^O";
+       if($os=~/darwin/io){$job_val=`sysctl -n hw.ncpu`;}
+       else{$job_val=`nproc`;}
+       chomp $job_val;
+       if ($job_val!~/^[0-9]+$/o){$job_val="0";}
+       }
+     $arg.=" '-j' '${job_val}'";
+     }
    my $makecmd=$self->{GMAKECMD}.$self->{CMDOPTS}." -f $makefile $arg";
    my $errfile=$ENV{SCRAM_INTwork}."/build_error";
    unlink($errfile);
