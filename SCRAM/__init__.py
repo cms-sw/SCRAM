172e9c20399682aef27fb542b2a4bd08fd9d5c9b
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2019-02-06 12:24:17 +0100

    setup using python-project-template

---
+++ SCRAM/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""SCRAM - A Software Configuration, Release and Management tool which supports configuration management and management processes for software development."""
+
+from SCRAM import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
