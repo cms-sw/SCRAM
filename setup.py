86f196fc03701f8bcd001a39707239a46d70280b
Yueh-Shun Li <shamrocklee@posteo.net>
2024-07-24 20:12:11 +0800

    setup.py: use strings instead of bytestrings for filenames

--- setup.py
+++ setup.py
@@ -185,7 +185,7 @@ def _lint():
     # - The result of subprocess call outputs are byte strings, meaning we need
     #   to pass a byte string to endswith.
     project_python_files = [filename for filename in get_project_files()
-                            if filename.endswith(b'.py')]
+                            if filename.endswith('.py')]
     retcode = subprocess.call(
         ['flake8', '--max-complexity=10'] + project_python_files)
     if retcode == 0:
